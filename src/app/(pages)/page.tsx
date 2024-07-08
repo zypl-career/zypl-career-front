3659a53abe0742349f6ead14e35ec510825d138b
ssabrinadev <babajanovasabrina4@gmail.com>
2024-07-08 12:04:45 +0500

    PROPS_BUG: bug in cards

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,10 +1,16 @@
-import { Hero, Navbar } from '@/entities';
+import { Hero, Navbar, ToolsInspiration } from '@/entities';
+import { CareerIcon } from '@/shared';
 
 const HomePage = () => {
   return (
     <div className="overflow-x-hidden">
       <Navbar />
       <Hero />
+      <ToolsInspiration
+        Icons={CareerIcon}
+        title="Мой карерный путь"
+        description="Мы аккумулируем большое количество онлайн-курсов по различным направлениям, позволяя сравнивать их, и выбирать то, что вам нравится"
+      />
     </div>
   );
 };
--- src/entities/cards/ui/tools-inspiration/ui.tsx
+++ src/entities/cards/ui/tools-inspiration/ui.tsx
@@ -1,7 +1,18 @@
-export const ToolsInspiration = () => {
+'use client';
+
+import React, { FC } from 'react';
+import { ToolsInspirationProps } from '.';
+
+export const ToolsInspiration: FC<ToolsInspirationProps> = ({
+  Icons,
+  title,
+  description,
+}) => {
   return (
-    <div>
-      <p>asd</p>
+    <div className="border border-gray-200 w-full md:w-[413px] md:p-[32px] rounded-xl">
+      <Icons />
+      <p className="md:text-[20px] font-bold pb-8">{title}</p>
+      <p className="text-[#7E7E7E]">{description}</p>
     </div>
   );
 };
