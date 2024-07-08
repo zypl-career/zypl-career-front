c508fdcbd58faa233bfa4e81ca588a2b61c460b2
ssabrinadev <babajanovasabrina4@gmail.com>
2024-07-08 13:27:00 +0500

    DURATION: cards

--- src/entities/cards/ui/tools-inspiration/ui.tsx
+++ src/entities/cards/ui/tools-inspiration/ui.tsx
@@ -9,7 +9,7 @@ export const ToolsInspiration: FC<ToolsInspirationProps> = ({
   description,
 }) => {
   return (
-    <div className="border border-gray-200 w-full md:w-[490px] p-5 md:p-[32px] my-2 rounded-xl">
+    <div className="border border-gray-200 w-full md:w-[490px] p-5 md:p-[32px] my-2 rounded-md transition-transform transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl cursor-pointer">
       <Icons />
       <p className="md:text-[20px] font-bold pb-8">{title}</p>
       <p className="text-[#7E7E7E]">{description}</p>
