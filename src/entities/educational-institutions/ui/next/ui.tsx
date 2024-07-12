a34faef69b326b4b04e65632088d260a5771b737
ssabrinadev <babajanovasabrina4@gmail.com>
2024-07-12 13:30:55 +0500

    fixed

--- src/entities/educational-institutions/ui/next/ui.tsx
+++ src/entities/educational-institutions/ui/next/ui.tsx
@@ -2,9 +2,9 @@ import { NextIcon } from '@/shared';
 
 export const NextEducational = ({ title }) => {
   return (
-    <div className="bg-white flex justify-between h-[72px] items-center cursor-pointer px-5 rounded-xl my-2 mx-4  transform transition-transform duration-300 ease-in-out active:scale-75">
+    <div className="bg-white flex justify-between py-8 items-center cursor-pointer px-5 rounded-xl my-2 mx-4  transform transition-transform duration-300 ease-in-out active:scale-75">
       <p>{title}</p>
-      <NextIcon />
+      <NextIcon className="flex-shrink-0" />
     </div>
   );
 };
--- src/entities/educational-institutions/ui/sections/next/ui.tsx
+++ src/entities/educational-institutions/ui/sections/next/ui.tsx
@@ -3,7 +3,7 @@ import { UNIVERSITY_TITLES } from '.';
 
 export const NextSections = () => {
   return (
-    <div className="lg:px-36">
+    <div className="lg:px-36 pb-56">
       {UNIVERSITY_TITLES.map((title, index) => (
         <NextEducational key={index} title={title} />
       ))}
