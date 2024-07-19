96193d58372bcd4295e18f29a8344abe0201bf06
ssabrinadev <babajanovasabrina4@gmail.com>
2024-07-19 11:05:19 +0500

    FEAT: content

--- src/app/(pages)/about-us/page.tsx
+++ src/app/(pages)/about-us/page.tsx
@@ -1,11 +1,13 @@
 import { AboutUsHero, Navbar } from '@/entities';
 import { Footer } from '@/widgets';
+import {OurMissions} from "@/entities/about-us/ui/sections/our-missions/ui";
 
 const AboutUsPage = () => {
   return (
-    <div className="min-h-screen">
+    <div className="bg-gray-100 min-h-screen overflow-x-hidden">
       <Navbar />
       <AboutUsHero />
+        <OurMissions />
       <Footer />
     </div>
   );
