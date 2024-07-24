287d3be30aa39502934c2db80226e8cd4a5b2a5d
ssabrinadev <babajanovasabrina4@gmail.com>
2024-07-24 12:34:46 +0500

    BUTTONS: added

--- src/entities/interests/ui/cards/ui.tsx
+++ src/entities/interests/ui/cards/ui.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
 import { CARDS_DATA_INTERES } from '.';
+import { Button } from '@/shared';
 
 export const CardsInteres = () => {
   return (
-    <section className="px-10 lg:px-80 pb-40">
+    <section className="px-10 lg:px-80 ">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5 cursor-pointer ">
         {CARDS_DATA_INTERES.map((card, index) => (
           <div
@@ -18,6 +19,20 @@ export const CardsInteres = () => {
             </h2>
           </div>
         ))}
+      </div>
+
+      <div className="flex flex-col sm:flex-row items-center mt-8 mb-40">
+        <Button variant="ghost" className="order-2 sm:order-1">
+          Отменить
+        </Button>
+        <Button
+          variant="subscribe"
+          showRightArrowIcon={true}
+          size="subscribe"
+          className="order-1 sm:order-2"
+        >
+          Продолжить
+        </Button>
       </div>
     </section>
   );
