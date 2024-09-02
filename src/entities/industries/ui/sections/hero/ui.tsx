919fd81193ba671a0f5ff41c6c3f2aa3927e9377
ssabrinadev <babajanovasabrina4@gmail.com>
2024-09-02 17:12:26 +0500

    fix

--- src/entities/industries/ui/sections/hero/ui.tsx
+++ src/entities/industries/ui/sections/hero/ui.tsx
@@ -2,7 +2,7 @@ import { Industries } from '@/shared';
 
 export const IndustriesHero = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-between bg-[#F8F8F8] px-20">
+    <div className="flex flex-col md:flex-row justify-between bg-[#F8F8F8] lg:px-20 px-1">
       <div className="py-5 px-2  md:py-32">
         <p className="text-2xl md:text-5xl md:w-[522px] font-bold md:pb-8">
           Отрасли
@@ -13,7 +13,7 @@ export const IndustriesHero = () => {
           в том числе о ведущих профессиях и перспективах роста.
         </p>
       </div>
-      <div className="lg:w-96 py-24">
+      <div className="lg:w-96 py-24 flex justify-center">
         <Industries />
       </div>
     </div>
--- src/entities/navbar/model/constans.ts
+++ src/entities/navbar/model/constans.ts
@@ -2,7 +2,6 @@ import { TNavbarItem } from '@/entities';
 
 export const NavbarData: TNavbarItem[] = [
   { title: 'Главная', link: '/' },
-  { title: 'Студенты', link: '/students' },
   { title: 'Учителя и практикующие профессию', link: '/teachers' },
   { title: 'Родители', link: '/parents' },
   { title: 'О нас', link: '/about-us' },
