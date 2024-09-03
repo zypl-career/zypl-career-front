bf8b54c5ff1f8ce210e3b91499b7435ecfbed8d2
ssabrinadev <babajanovasabrina4@gmail.com>
2024-09-03 06:05:47 +0500

    REFACTORED: code

--- src/entities/industries/ui/sections/hero/ui.tsx
+++ src/entities/industries/ui/sections/hero/ui.tsx
@@ -2,20 +2,20 @@ import { Industries } from '@/shared';
 
 export const IndustriesHero = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-between bg-[#F8F8F8] lg:px-20 px-1">
-      <div className="lg:w-96 py-24 flex justify-center order-1 md:order-2">
-        <Industries />
+    <section className="bg-[#F8F8F8]">
+      <div className="container flex flex-col md:flex-row justify-between">
+        <div className="lg:w-96 py-24 flex justify-center order-1 md:order-2">
+          <Industries />
+        </div>
+        <div className="py-5 md:py-32 order-2 md:order-1">
+          <p className="text-2xl md:text-5xl font-bold md:pb-8">Отрасли</p>
+          <p className="md:w-[522px] text-gray-600">
+            Изучите различные отрасли, в которых вы можете оказать влияние.
+            Ознакомьтесь с обзорами отрасли, чтобы узнать больше о каждой
+            отрасли, в том числе о ведущих профессиях и перспективах роста.
+          </p>
+        </div>
       </div>
-      <div className="py-5 px-2 md:py-32 order-2 md:order-1">
-        <p className="text-2xl md:text-5xl md:w-[522px] font-bold md:pb-8">
-          Отрасли
-        </p>
-        <p className="md:w-[522px] text-gray-600">
-          Изучите различные отрасли, в которых вы можете оказать влияние.
-          Ознакомьтесь с обзорами отрасли, чтобы узнать больше о каждой отрасли,
-          в том числе о ведущих профессиях и перспективах роста.
-        </p>
-      </div>
-    </div>
+    </section>
   );
 };
