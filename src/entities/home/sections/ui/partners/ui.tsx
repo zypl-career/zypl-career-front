8e9023aa7f2471eb6c08b81a0afbc4842c5646a1
ssabrinadev <babajanovasabrina4@gmail.com>
2024-09-03 06:50:09 +0500

    fix

--- src/entities/home/sections/ui/partners/ui.tsx
+++ src/entities/home/sections/ui/partners/ui.tsx
@@ -4,7 +4,7 @@ import { FC } from 'react';
 export const Partners: FC<TPartnerProps> = ({ data }) => {
   return (
     <section className="bg-[#F8F8F8]">
-      <div className="container pt-10 pb-28 lg:py-32 theme:bg-primaryBg theme:text-primary">
+      <div className="container pt-10 pb-28 theme:bg-primaryBg theme:text-primary">
         <h2 className="lg:text-2xl font-bold">Партнеры</h2>
         <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2">
           {data.map((partner, index) => (
