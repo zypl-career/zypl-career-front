513209c4dc332f9bb03837dc124447a28ccc4463
ssabrinadev <babajanovasabrina4@gmail.com>
2024-09-05 17:01:58 +0500

    fix

--- src/entities/teachers/ui/resources/ui.tsx
+++ src/entities/teachers/ui/resources/ui.tsx
@@ -3,7 +3,7 @@ import { CardSectionTeachers } from '@/entities';
 export const ResourcesTeachers = () => {
   return (
     <div className="px-5 lg:px-28 theme:bg-primaryBg theme:text-primary theme:border theme:border-primary">
-      <h3 className="font-bold text-2xl">Ресурсы для родителей</h3>
+      <h3 className="font-bold text-2xl">Учебные ресурсы</h3>
       <p className="text-gray-600 theme:text-primary">
         Откройте для себя ресурсы, призванные помочь вашим детям повысить
         осведомленность о карьерном росте.
