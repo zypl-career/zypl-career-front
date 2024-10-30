fcdb4f619c36f513c113d1192594702a62449ec6
ssabrinadev <babajanovasabrina4@gmail.com>
2024-10-30 19:33:58 +0500

    translate

--- src/entities/home/sections/ui/partners/ui.tsx
+++ src/entities/home/sections/ui/partners/ui.tsx
@@ -1,11 +1,16 @@
+'use client';
+
 import { PartnersCard, type TPartnerProps } from '@/entities';
 import { FC } from 'react';
+import { useTranslations } from 'next-intl';
 
 export const Partners: FC<TPartnerProps> = ({ data }) => {
+  const t = useTranslations('Partners');
+
   return (
     <section className="bg-[#F8F8F8]">
       <div className="container pt-10 pb-28 theme:bg-primaryBg theme:text-primary">
-        <h2 className="lg:text-2xl font-bold">Партнеры</h2>
+        <h2 className="lg:text-2xl font-bold">{t('heading')}</h2>
         <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2">
           {data.map((partner, index) => (
             <PartnersCard
