1c187c7949f9a7fbac0719056e11e9a3241f7dae
ssabrinadev <babajanovasabrina4@gmail.com>
2024-11-05 13:48:43 +0500

    feat(translate): added translate

--- src/entities/choosing-values/ui/main/ui.tsx
+++ src/entities/choosing-values/ui/main/ui.tsx
@@ -3,17 +3,19 @@
 import { Button } from '@/shared';
 import { CardChoosingValues } from '@/entities/choosing-values/ui';
 import { useState } from 'react';
+import { useTranslations } from 'next-intl';
 import { useRouter } from 'next/navigation';
 
 export const MainChoosingValues = () => {
   const [disableContinueButton, setDisableContinueButton] = useState('');
+  const t = useTranslations('valuesSurveys');
   const router = useRouter();
   return (
     <div>
       <CardChoosingValues onSelect={setDisableContinueButton} />
       <div className="flex flex-col sm:flex-row items-center mt-8 mb-40">
         <Button variant="ghost" className="order-2 sm:order-1">
-          Отменить
+          {t('cancel')}
         </Button>
         <Button
           variant="subscribe"
@@ -22,7 +24,7 @@ export const MainChoosingValues = () => {
           disabled={!disableContinueButton}
           onClick={() => router.push('your-result')}
         >
-          Далее
+          {t('next')}
         </Button>
       </div>
     </div>
