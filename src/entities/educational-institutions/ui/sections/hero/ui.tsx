3576111fad26e884ec37ddfbbe1bf3d5eeca87bd
ssabrinadev <babajanovasabrina4@gmail.com>
2024-11-11 14:06:16 +0500

    feat(educational): trabslated educational page

--- src/entities/educational-institutions/ui/sections/hero/ui.tsx
+++ src/entities/educational-institutions/ui/sections/hero/ui.tsx
@@ -1,27 +1,26 @@
 import Image from 'next/image';
 import { Universities } from '@/shared';
+import { useTranslations } from 'next-intl';
 
 export const HeroEducational = () => {
+  const t = useTranslations('heroEducational');
+
   return (
     <div className="bg-white">
       <div className="container flex flex-col md:flex-row justify-between">
         <div className="lg:w-96 pt-24 flex justify-center order-1 md:order-2">
           <Image
             src={Universities}
-            alt="educational"
+            alt={t('imageAlt')}
             className="w-52 lg:w-72 lg:h-72"
           />
         </div>
         <div className="py-5 md:py-32 order-2 md:order-1">
           <p className="text-2xl md:text-5xl md:w-[522px] font-bold md:pb-8">
-            Вузы и колледжи
+            {t('title')}
           </p>
           <p className="md:w-[522px] text-gray-600">
-            В 2022 году по результатам ЦВЭ должен осуществиться приём студентов
-            в 39 образовательных учреждений высшего профессионального
-            образования (18 университетов, 1 академия, 14 институтов, 6 филиалов
-            университетов и институтов) и 70 образовательных учреждений среднего
-            профессионального образования.
+            {t('description')}
           </p>
         </div>
       </div>
--- src/entities/educational-institutions/ui/sections/university/ui.tsx
+++ src/entities/educational-institutions/ui/sections/university/ui.tsx
@@ -1,10 +1,14 @@
+import { useTranslations } from 'next-intl';
+
 export const UniversitySection = () => {
+  const t = useTranslations('universitySection');
+
   return (
     <div>
       <div className="flex justify-center">
         <p className="text-center text-3xl lg:w-[950px] leading-9 text-gray-600 py-16">
-          Образовательные учреждения высшего профессионального образования{' '}
-          <span className="font-bold text-black">Душанбе</span>
+          {t('description')}{' '}
+          <span className="font-bold text-black">{t('city')}</span>
         </p>
       </div>
     </div>
