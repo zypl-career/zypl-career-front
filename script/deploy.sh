4d8f50fb3aa31afb6a6f76e4fc8a81d8e9ad98e3
shoh <behruzrahimov580@gmail.com>
2025-02-19 18:18:48 +0500

    ci cd

---
+++ script/deploy.sh
@@ -0,0 +1,12 @@
+# Remove all unused data
+sudo docker system prune -af # remove all unused data
+sudo docker volume prune -f # remove all unused volumes
+sudo docker network prune -f # remove all unused networks
+sudo docker image prune -af # remove all unused images
+sudo docker container prune -f # remove all stopped containers
+sudo docker builder prune -f # remove all unused builders
+
+# Update the repository and redeploy
+git pull origin ci_cd
+docker-compose down
+docker-compose up -d --build
