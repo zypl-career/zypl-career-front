81be1597f66d57e68cbde376df4f4ccdf0a2ab83
shoh <behruzrahimov580@gmail.com>
2025-02-19 19:19:36 +0500

    fixed script

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,14 +2,14 @@
 cd ../
 
 # Remove all unused data
-sudo docker system prune -af # remove all unused data
-sudo docker volume prune -f # remove all unused volumes
-sudo docker network prune -f # remove all unused networks
-sudo docker image prune -af # remove all unused images
-sudo docker container prune -f # remove all stopped containers
-sudo docker builder prune -f # remove all unused builders
+docker system prune -af # remove all unused data
+docker volume prune -f # remove all unused volumes
+docker network prune -f # remove all unused networks
+docker image prune -af # remove all unused images
+docker container prune -f # remove all stopped containers
+docker builder prune -f # remove all unused builders
 
 # Update the repository and redeploy
 git pull origin ci_cd
-sudo docker compose down
-sudo docker compose up -d --build 
+docker compose down
+docker compose up -d --build 
